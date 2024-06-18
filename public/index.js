f3aef7755d4bf4888ebb7c4e14a37a88e3dfbd58
Maia-Aedo <aedomaia10@gmail.com>
2024-06-18 16:01:20 -0300

    recibiendo mensajes del servidor

--- public/index.js
+++ public/index.js
@@ -1,3 +1,26 @@
 // inicializamos una constante
-const socket = io() // ya podemos empazar a usar los sockets desde el cliente
+let socket = io.connect() // ya podemos empazar a usar los sockets desde el cliente
 
+// recibimos el mensaje
+// socket.on('notificacion', data => {
+//     console.log(data)
+// })
+
+// data tiene el array de mensajes que envia el servidor 
+socket.on('messages', function(data){
+    console.log(data)
+})
+
+// creamos funcion que se encargara de pintar en el html los mensajes
+// esta funcion itera sobre los datos que llegan a traves del socket con funcion map de js
+// para cada elemento pinta una plantilla html con el nombre del autor y texto del msj de cada elto
+function render(data){
+    // index
+    var html = data.map(function(elem){
+        return(`<div>
+                <strong>${elem.author}</strong>:
+                <em>${elem.text}</em></div>`)
+    }).join("");
+    element.getElementById('messages').innerHTML = html;
+}
+socket.on('messages', function(data){ render(data); });
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 // importaciones
 const express = require('express');
-const io = require('socket.io');
-
 // inicializamos express
 const app = express();
 // importamos http para que los sockets funcionen
 // http recibe la constante de express
 const http = require('http').Server(app);
 // io recibe la constante de http
-const io = require('socket.io') (http);
+const io = require('socket.io')(http);
+// la aplicacion que creemos con express, la pasamos a servidor http
+const server = require('http').Server(app);
+
+// arreglo con los mensajes que queremos mostrar
+let messages = [
+    { author: "juan", text: "hola, que tal?" },
+    { author: "pedro", text: "muy bien" },
+    { author: "ana", text: "genial" }
+]
+
+// el servidor websocket debe estar atento a que se realice la conexion
+io.on('connection', function (socket) {
+    console.log('un cliente se ha conectado');
+    socket.emit('messages', messages)
+})
 
 /* hay que configurar el middleware para dejar disponibles las rutas y archivos estáticos.
 de esta manera el archivo indes.html va a mostrar cuando ingresemos a la página
@@ -18,47 +31,46 @@ EL SERVIDOR SE ARRANCA CON HTTP.LISTEN() */
 app.use(express.static('./public'));
 // esta ruta va a cargar el index.html en la raiz
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname})
+    res.sendFile('index.html', { root: __dirname })
 })
 
 // servidor funcionando en puerto 3000
-http.listen(3000, () => console.log('SERVER ON'))
+// http.listen(3000, () => console.log('SERVER ON'))
 
 // connection se ejecuta la primera vez que se abre una nueva conexion
-io.on('connection', (socket) => {
-    // solo se imprime la primera vez que se abre la conexion
-    console.log('usuario conectado')
-})
+// io.on('connection', (socket) => {
+//     // solo se imprime la primera vez que se abre la conexion
+//     console.log('usuario conectado')
+// })
 
 // envio de datos al cliente desde el servidor
 io.on('connection', socket => {
     console.log('usuario conectado')
     // el metodo emit permite enviar mensaje desde el servidor al cliente,
     // el primer parametro que recibe es el nombre del evento y el segundo la info que se transmite
-    socket.emit('mi mensaje', 'este es mi mensaje desde el servidor')
-})
-
-// recepcion de datos del servidor desde cliente
-socket.on('mi mensaje', data => {
-    alert(data)
-    socket.emit('noticacion', 'mensaje recibido exitosamente')
-})
-
-// recibimos el mensaje
-socket.on('notificacion', data => {
-    console.log(data)
+    socket.emit('mi mensaje', 'este es mi mensaje desde el servidor');
+    // recepcion de datos del servidor desde cliente
+    socket.on('mi mensaje', data => {
+        alert(data)
+        socket.emit('noticacion', 'mensaje recibido exitosamente')
+    })
 })
 
 // envio de datos a todos los clientes conectados desde servidor
-io.on('connection', socket =>{
+io.on('connection', (socket) => {
     console.log('nuevo cliente conectado');
     // envio mensajes al cliente que se conecto
     socket.emit('mensajes', mensajes);
     // escucho los mensajes enviados por el cliente y se lo propago a todos
     socket.on('mensaje', data => {
-        mensajes.push({socketid: socket.id, mensaje: data})
+        mensajes.push({ socketid: socket.id, mensaje: data })
         // enviamos mensaje global a todos los clientes conectados al canal de websocket
         io.sockets.emit('mensajes', mensajes)
     })
+})
+
+// servidor
+server.listen(8080, () => {
+    console.log('servidor corriendo en http://localhost:8080');
 })
 
